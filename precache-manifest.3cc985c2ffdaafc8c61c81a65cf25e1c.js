440ffe1c7d84809d47877c6869b1d62237bd54e9
marm007 <dhkm.gornica@poczta.fm>
2019-07-01 14:29:49 +0200

    Updates

---
+++ precache-manifest.3cc985c2ffdaafc8c61c81a65cf25e1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f5c2ef2326093908bc9828aa3684c062",
+    "url": "/filmapp_frontend/static/media/ico.f5c2ef23.png"
+  },
+  {
+    "revision": "edab0e1695f40bbacc18b092a1af8650",
+    "url": "/filmapp_frontend/static/media/error-404.edab0e16.png"
+  },
+  {
+    "revision": "280ed588460cab8b7bf9",
+    "url": "/filmapp_frontend/static/js/runtime~main.fbaaed63.js"
+  },
+  {
+    "revision": "42b83f5023f8d757a439",
+    "url": "/filmapp_frontend/static/js/main.d22c80cc.chunk.js"
+  },
+  {
+    "revision": "2cf1429a1e86471fbedd",
+    "url": "/filmapp_frontend/static/js/2.5e1319db.chunk.js"
+  },
+  {
+    "revision": "42b83f5023f8d757a439",
+    "url": "/filmapp_frontend/static/css/main.4eab46b3.chunk.css"
+  },
+  {
+    "revision": "2cf1429a1e86471fbedd",
+    "url": "/filmapp_frontend/static/css/2.bfa25b4c.chunk.css"
+  },
+  {
+    "revision": "ff7833921286060754caddd84801a3b2",
+    "url": "/filmapp_frontend/index.html"
+  }
+];
