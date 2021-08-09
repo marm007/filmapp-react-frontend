4e114c0c9e41617817c61f108f94190a6d641070
marm007 <dhkm.gornica@poczta.fm>
2021-08-09 13:09:34 +0200

    Changes

--- src/helpers/axios.js
+++ src/helpers/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL + process.env.REACT_APP_PATH_NAME
+    baseURL: process.env.REACT_APP_API_URL.slice(0, -1) + process.env.REACT_APP_PATH_NAME
 })
 export default instance
