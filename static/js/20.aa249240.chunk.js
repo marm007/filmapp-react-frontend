f1313fcd71acdd1d56aa905c3d565ba33be4fde1
marm007 <dhkm.gornica@poczta.fm>
2021-10-06 15:51:23 +0200

    Updates

---
+++ static/js/20.aa249240.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfilmapp_frontened=this.webpackJsonpfilmapp_frontened||[]).push([[20],{507:function(n,t,e){"use strict";e.r(t);e(1),e(696);var p=e(2);t.default=function(){return Object(p.jsx)(p.Fragment,{})}},696:function(n,t,e){}}]);
+//# sourceMappingURL=20.aa249240.chunk.js.map
---
+++ static/js/21.a0907485.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfilmapp_frontened=this.webpackJsonpfilmapp_frontened||[]).push([[21],{506:function(n,t,e){"use strict";e.r(t);e(1),e(695);var p=e(2);t.default=function(){return Object(p.jsx)(p.Fragment,{})}},695:function(n,t,e){}}]);
+//# sourceMappingURL=21.a0907485.chunk.js.map
